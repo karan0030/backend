2875319054fdcc4584eadc82410d84a7a564eeda
karan0030 <62276712+karan0030@users.noreply.github.com>
2020-12-24 23:28:20 +0530

    Update product.js
    removed limit

--- controllers/product.js
+++ controllers/product.js
@@ -134,7 +134,7 @@ exports.getAllUniqueCategory =(req,res)=>{
 }
 
 exports.getAllProduct =(req,res)=>{
-    let limit = req.query.limit? parseInt(req.query.limit):8
+    let limit = req.query.limit? parseInt(req.query.limit)
     let sortBy =req.query.sortBy ?req.query.sortBy:"_id"
     Product.find()
     .select("-photo")
